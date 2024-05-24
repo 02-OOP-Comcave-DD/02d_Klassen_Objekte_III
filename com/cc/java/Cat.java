67d250b15501f76e5899bdc35efe6eb4aeb028bd
Rene <media-r.r@online.de>
2024-05-24 12:45:55 +0200

    02 / Tomcat und Cat getrennt

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,15 +5,13 @@ public class Cat {
   private String name;
   private String furColor;
   private int age; 
-  private boolean isFemale;
 
   private int counter;
 
-public Cat(String name, String furColor, int age, boolean isFemale) {
+public Cat(String name, String furColor, int age) {
     this.name = name;
     this.furColor = furColor;
     this.age = age;
-    this.isFemale = isFemale;
   }
 
 public String getStringAttributes(String op) {
@@ -28,12 +26,8 @@ public String getStringAttributes(String op) {
 }
 
 public String getAge() {
-  if (isFemale) {
     return checkCompliance();
-  } else {
-    return String.valueOf(age);
   }
-}
 
 private String checkCompliance() {
 
